ec863807422ae1134833ab4b01097b23ec2b9bba
cindy-pan-ics4u1-02-25 <cindy.pan@student.tdsb.on.ca>
2025-02-26 15:55:41 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -2,8 +2,33 @@ import java.util.*;
 
 public class PracticeProblem {
 
-	public static void main(String args[]) {
 
+	public static void swap(ArrayList<String> list , int one, int two){
+		list.set((one), list.get(two));
+		list.set(two, list.get(one));
+	}	
+
+	public static ArrayList<Double> createArrayList(double[] list){
+		ArrayList<Double> arrL = new ArrayList<Double>();
+		for (int length = 0; length< list.length; length++){
+			arrL.set(length, list[length]);
+		}
+		return arrL;
 	}
 
+	public static HashMap<String, Integer>  combineParallelArrays(String[] names, int[] ages){
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		if (names.length == ages.length){
+			for (int length = 0; length<ages.length; length++){
+				map.put(names[length],ages[length]);
+			}
+
+		}		
+		return map;
+	}
+	
+	public static void increaseAge(HashMap<String, Integer> map, ) 
+
+
 }
+
